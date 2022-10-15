50d6c6aa5450edd672eade819a708eb1345534c6
Fabio Souza <fs.embarcados@gmail.com>
2022-10-15 13:15:07 -0300

    incluindo ifndef

--- pwm/pwm.c
+++ pwm/pwm.c
@@ -1,5 +1,5 @@
 /**
- * @file pwm_generator.c
+ * @file pwm.c
  * @author Fabio Souza (contato@fabiosouza.org)
  * @brief Exemplo básico para configuração do PWM
  * @version 0.1
@@ -13,6 +13,9 @@
 #include <stdio.h>
 #include "hardware/pwm.h"
 
+
+#define CALC_FREQ
+
 #define PWM_OUT_1 4       //PWM A
 #define PWM_OUT_2 5       //PWM B
 
@@ -25,10 +28,12 @@ uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d){
     pwm_set_clkdiv_int_frac(slice_num,divisor16/16,divisor16&0xF);
     pwm_set_wrap(slice_num,wrap);
     pwm_set_chan_level(slice_num,chan,wrap*d/100);
+    return wrap;
 }
 
 int main() {
     
+    #ifndef CALC_FREQ
     gpio_set_function(PWM_OUT_1, GPIO_FUNC_PWM);        //Seleciona PWM para o pino do LED
     uint slice_num = pwm_gpio_to_slice_num(PWM_OUT_1);  //pega o slice referente ao pino
     pwm_set_wrap(slice_num,500);
@@ -36,23 +41,26 @@ int main() {
     pwm_set_enabled(slice_num,true);                    //habilita PWM
 
     while(1);
+    
 
-/*
+    #else
     //configuração do GPIO
     gpio_set_function(PWM_OUT_1, GPIO_FUNC_PWM);        //Seleciona PWM para o pino do LED
     uint slice_num = pwm_gpio_to_slice_num(PWM_OUT_1);  //pega o slice referente ao pino   
-    uint chan = pwm_gpio_to_channel(PWM_OUT_1);        //pega canal
+    uint chan = pwm_gpio_to_channel(PWM_OUT_1);        //pegar canal
     
     //cofiguração do PWM
-    uint32_t wrap = pwm_set_freq_duty(slice_num, chan, 50, 50);
+    uint32_t wrap = pwm_set_freq_duty(slice_num, chan, 100, 50);
     pwm_set_enabled(slice_num,true);                    //habilita PWM
-    
-    
 
-    while (1)
-        for(int i = 0;i<wrap;i++){
+    while(1);        
+
+    while (1){
+        uint32_t i;
+        for(i = 0;i<wrap;i++){
             pwm_set_gpio_level(PWM_OUT_1, i);
             sleep_ms(1);
         }
-        */
+    }
+    #endif
 }
